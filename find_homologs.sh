2ce4a408d0f3f112bb8e693c351cd6b74f953533
rabab-fatma <rabab.fatma2810@gmail.com>
2024-09-15 14:26:24 -0400

    Modified find_homologs.sh to perform BLAST search and filter results

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,5 +4,29 @@
 #echo "Number of perfect matches: $(wc -l <"$3")"
 
 #Changing file content
-echo "oops!"
+#echo "oops!"
 
+#!/bin/bash
+
+# Usage: ./find_homologs.sh <query file> <subject file> <output file>
+# Example: ./find_homologs.sh query.faa subject.fna output.txt
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./find_homologs.sh <query file> <subject file> <output file>"
+    exit 1
+fi
+
+query_file=$1
+subject_file=$2
+output_file=$3
+
+# Perform BLAST search (tblastn: protein query, nucleotide subject)
+blast_out="blast_results.txt"
+tblastn -query "$query_file" -subject "$subject_file" -out "$blast_out" -outfmt "6 qseqid sseqid pident length qlen slen"
+
+# Filter hits (pident > 30%, length > 90% of query length)
+awk '$3 > 30 && $4 > 0.9 * $5' "$blast_out" > "$output_file"
+
+# Count and print the number of matches
+num_matches=$(wc -l < "$output_file")
+echo "Number of matches identified: $num_matches"
