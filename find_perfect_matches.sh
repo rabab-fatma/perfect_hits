76f0649019da35fc2f4b232b8034ef9130ae50e4
rabab.fatma2810@gmail.com <rabab@LAPTOP-4UHQIRK4>
2024-09-14 20:33:14 -0400

    find_perfect_matches.sh

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -1,5 +1,8 @@
-m
+
 #Running BLAST with the query and subject files
-blastn -query "$1" -subject "$2" -outfmt "6" | awk '$3==100' > "$3"
-echo "Number of perfect matches: $(wc -l <"$3")"
+#blastn -query "$1" -subject "$2" -outfmt "6" | awk '$3==100' > "$3"
+#echo "Number of perfect matches: $(wc -l <"$3")"
+
+#Changing file content
+echo "oops!"
 
